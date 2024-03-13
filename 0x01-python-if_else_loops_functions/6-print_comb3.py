8f8f0d592cd4977f9c6cd4692f71088736fa2d5b
gabrielnoah129@gmail.com <Geff115>
2024-03-13 13:39:46 +0100

    Different combination of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        print("{:2d}{:2d}".format(i, j), end = "\n" if i == 8 and j == 9 else ", ")
+        print("{:d}{:d}".format(i, j), end = "\n" if i == 8 and j == 9 else ", ")
