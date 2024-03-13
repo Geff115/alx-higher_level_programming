adea38a579867551c804c9cb82748fff654ee6c2
gabrielnoah129@gmail.com <Geff115>
2024-03-13 15:57:00 +0100

    Checking for lowercase alphabets

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 def islower(c):
-    if c >= "a" and c <= "z":
-        return True
-    else:
+    if c.isdigit():
         return False
+    elif ord(c) >= 32 and ord(c) <= 47:
+        return False
+    elif ord(c) >= 58 and ord(c) <= 64:
+        return False
+    elif ord(c) >= 91 and ord(c) <= 96:
+        return False
+    elif ord(c) >= 123 and ord(c) <= 126:
+        return False
+    else:
+        return 97 <= ord(c) <= 122
+
+
 print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
