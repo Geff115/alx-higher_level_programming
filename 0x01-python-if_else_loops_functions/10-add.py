b0ebade0cabfafe9ec68e5fe9cc9a091ad8875d2
gabrielnoah129@gmail.com <Geff115>
2024-03-14 00:12:16 +0100

    A function that adds two integers

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -2,5 +2,7 @@
 def add(a, b):
     if isinstance(a, int) and isinstance(b, int):
         return a + b
+    elif isinstance(a, int) and isinstance(b, str):
+        return "Error"
     else:
         return "Enter only two integers to add"
