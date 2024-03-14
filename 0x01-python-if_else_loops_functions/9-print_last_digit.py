a6940443bc6638a5ba723646bb1cab58ffbb8576
gabrielnoah129@gmail.com <Geff115>
2024-03-14 01:50:39 +0100

    Last digit of a number

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_digit = abs(number) % 10
-    return last_digit
+    return f"{last_digit}"
+
+
+print(print_last_digit(98))
+print(print_last_digit(0))
+r = print_last_digit(-1024)
+print(r)
