844d40422c66f99bdfb5bbc12b31d8b787c48931
gabrielnoah129@gmail.com <Geff115>
2024-04-04 00:36:49 +0100

    Update in the empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+
+
+"""Define a class Square."""
+
+
 class Square:
-    # Created a class named square.
-    pass
-
-
-if __name__ = "__main__":
-    # This block will only run when the script is run directly.
-    # It won't run when imported.
+    """Created a class named square."""
     pass
