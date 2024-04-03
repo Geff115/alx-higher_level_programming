4dfa15122e783347385888a5c3c5795edd6131c3
gabrielnoah129@gmail.com <Geff115>
2024-04-04 00:46:24 +0100

    Update in private instance of class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-class Square:  # A class called Square
 
-    def __init__(self, size):  # Initialization.
-        self.__size = size  # A private instance of the class.
+"""A class in python"""
 
 
-if __name__ = "__main__":  # Ensures my file doesn't get imported
-    pass
+class Square:
+
+    def __init__(self, size):
+
+        """Initialization."""
+        self.__size = size  """A private instance of the class"""
