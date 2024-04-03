7b0f426552cad56efd30ebf1e5f727d9aac10804
gabrielnoah129@gmail.com <Geff115>
2024-04-04 00:05:33 +0100

    Empty class that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 class Square:
+    # Created a class named square.
+    pass
+
+
+if __name__ = "__main__":
+    # This block will only run when the script is run directly.
+    # It won't run when imported.
     pass
