cf705e7bef07a9d833b5588f505d33311b4341ec
gabrielnoah129@gmail.com <Geff115>
2024-04-15 17:01:02 +0100

    A class with a area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This class has an area method"""
+
+
+class BaseGeometry:
+    """This is a class"""
+
+    def area(self):
+        """This is a method"""
+
+        raise Exception("area() is not implemented")
