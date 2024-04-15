fab241714422e436e62d26ba91e4caae7637f867
gabrielnoah129@gmail.com <Geff115>
2024-04-15 20:24:59 +0100

    A class BaseGeometry and doctest

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A Base Geometry class"""
+
+
+class BaseGeometry:
+    """This is a class"""
+
+    def area(self):
+        """This is a method"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """This is a method"""
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        self.value = value
