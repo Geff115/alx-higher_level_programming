eecd11400d91e3faf13e3c765b354ebbf3f7b623
Geff115 <gabrielnoah129@gmail.com>
2024-04-16 03:07:34 +0000

    more to be done

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""This is a square class that inherits
+from a rectangle class.
+"""
+
+
+class BaseGeometry:
+    """A BaseGeometry class"""
+
+    def area(self):
+        """Area method"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates name to be a positive integer
+        otherwise raise an error.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """This is a child class"""
+
+    def __init__(self, width, height):
+        """Initialization method for the child lass
+        with height and width as private attributes.
+        """
+
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """This is an area method that returns width * height"""
+
+        return (self.__height * self.__width)
+
+    def __str__(self):
+        """String representation method"""
+
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
