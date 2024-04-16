781963537744232b6c86f5b2f30a0d5606db865e
gabrielnoah129@gmail.com <Geff115>
2024-04-16 03:19:27 +0100

    A Rectangle class that inherits with area and str methods

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A rectangle class that inherits from a
+BaseGeometry class.
+"""
+
+
+import importlib
+
+BaseGeometry = importlib.import_module("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This is a child class"""
+
+    def __init__(self, width, height):
+        """Initialization method for the child lass
+        with height and width as private attributes.
+        """
+
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """This is an area method that returns width * height"""
+
+        return (self.__height * self.__width)
+
+    def __str__(self):
+        """String representation method"""
+
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
