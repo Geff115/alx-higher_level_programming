ba5c36d6d4de106d065de8e5b77249153c8d513e
gabrielnoah129@gmail.com <Geff115>
2024-04-17 16:58:00 +0100

    Retrieves a dictionary representation of a Student instance

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is a Python script"""
+
+
+class Student:
+    """This is a student class with public instance
+    attributes with first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization method of the public instances"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """A method that returns a dictionary representation of
+        a Student object.
+        """
+
+        return (vars(self))
