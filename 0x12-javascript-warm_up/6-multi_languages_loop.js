bde420948a9bf423a3bdd8337dfd8b0342ccbcf5
Geff115 <gabrielnoah129@gmail.com>
2024-06-19 23:07:19 +0100

    A script that uses a loop to print an array of strings

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// This script uses a loop to print an array
+// of strings.
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for (i = 0; i < 3; i++) {
+  console.log(lines[i]);
+}
