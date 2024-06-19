96ee8e57176aa91f68da41aaf0aed5ca52416505
Geff115 <gabrielnoah129@gmail.com>
2024-06-19 18:33:51 +0100

    A script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/*
+ * firstLine: This variable stores a string.
+ * secondLine: This variable stores a string.
+ * thirdLine: This variable stores a string.
+ */
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
