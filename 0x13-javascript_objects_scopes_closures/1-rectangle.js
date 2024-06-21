8828da562a0c2a5b21ee98a561292d2c12d54a1b
Geff115 <gabrielnoah129@gmail.com>
2024-06-21 02:27:07 +0100

    A class Rectangle that defines a rectangle. The constructore takes two arguments: w and h

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// This script creates a Rectangle class that defines a rectangle.
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
