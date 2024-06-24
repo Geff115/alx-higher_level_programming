7f9cd630f2713e6e8512e50a41eb1fc406e86a28
Geff115 <gabrielnoah129@gmail.com>
2024-06-24 04:39:46 +0100

    List all states objects and cities objects contained in the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -25,7 +25,7 @@ def list_all_states_and_cities():
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
