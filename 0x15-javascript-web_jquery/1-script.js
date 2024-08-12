13d42b509dc856a92d1cd129af033679e190c992
Geff115 <gabrielnoah129@gmail.com>
2024-08-12 07:52:39 +0100

    A JavaScript that updates the text color of the <header> element to red using jQuery. Included the jQuery api in the html file 1-main.html

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+/**
+ * This script updates the text color of the <header> element to red
+ * using jQuery api: https://code.jquery.com/jquery-3.2.1.min.js
+ */
+
+$("header").css("color", "#FF0000");
